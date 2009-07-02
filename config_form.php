83573300ea1e9fb5aab3f08d5143a8b0d2f5a67b
mohit.ed <mohit.ed@d187d172-3112-0410-8ee7-ad866068e984>
2009-07-02 08:05:35 +0000

    init creative commons chooser to allow for a unique license for each item
    git-svn-id: https://addons.omeka.org/svn/plugins/CreativeCommonsChooser/trunk@1466 d187d172-3112-0410-8ee7-ad866068e984

---
+++ config_form.php
@@ -0,0 +1,6 @@
+<div class="field">
+	<label for="default_license_uri">Default Creative Commons License Link:</label>
+	<div class="inputs">
+	<input type="text" class="textinput"  name="default_license_uri" size="60" value="<?php echo get_option('default_license_uri'); ?>" id="default_license_uri" />
+	</div>
+</div>
---
+++ views/public/js/index.php
@@ -0,0 +1,3 @@
+<?
+	include('complete.js')
+?>
